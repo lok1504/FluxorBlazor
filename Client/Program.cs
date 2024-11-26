8a465ef58beee702247fe25bd0827abdaa8cc19a
Yap Joon Lok <jl.yap@klk.com.my>
2024-11-26 15:53:31 +0800

    Install and set up Fluxor in client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FluxorBlazor.Client;
+using Fluxor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddFluxor(o => o.ScanAssemblies(typeof(Program).Assembly));
 
 await builder.Build().RunAsync();
