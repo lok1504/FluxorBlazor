582429363d22a85f8b166e4c9ab6d9c1e35aeda0
Yap Joon Lok <jl.yap@klk.com.my>
2024-11-27 15:46:12 +0800

    Add toaster component

--- Client/Shared/NavMenu.razor.cs
+++ Client/Shared/NavMenu.razor.cs
@@ -15,24 +15,10 @@ public partial class NavMenu : FluxorComponent
     [Inject]
     protected IState<WeatherState> WeatherState { get; set; }
 
-    [Inject]
-    protected IToastService ToastService { get; set; }
-
     private bool collapseNavMenu = true;
 
     private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
     private string WeatherItemClass => WeatherState.Value.Loading ? "font-weight-bold" : null;
-
-    protected override void OnInitialized()
-    {
-        SubscribeToAction<WeatherSetForecastsAction>(ShowWeatherToast);
-        base.OnInitialized();
-    }
-
-    private void ShowWeatherToast(WeatherSetForecastsAction action)
-    {
-        ToastService.ShowInfo("Weather Forecasts have been updated!");
-    }
 
     private void ToggleNavMenu()
     {
